885429af8575bd955dd71d17c54d5faa9874ea0f
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-05-17 22:37:35 -0600

    first commit

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var currentLoca = $("#current-location");
+var userCityInput = $("#user-city");
+// add eventlistner for usercitysubmit
+var userCitySubmit = $("user-city-submit");
+// use momentJS, display moment JS
+var previousCitiesUl = $("previous-cities");
+var futureForcast = $("future-forcast");
