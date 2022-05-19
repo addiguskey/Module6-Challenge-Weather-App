cd0a3e850484e2e89037cfc4e5d5106f8eb27dcb
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-05-18 22:31:02 -0600

    NOTHING IS WORKING

--- assets/script.js
+++ assets/script.js
@@ -11,13 +11,58 @@ var currentTemp = $("current-temp");
 var currentHum = $("current-humidity");
 var currentWind = $("current-wind");
 var currentUV = $("current-UV");
-// add eventlinster for submit btn
-//make an accnt for my key
 
-function getWeather() {
+//local storage for searched cities, search History
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+userCitySearchBtn.on("click", function () {
+  var searchedCity = userCityInput.val();
+  getWeather(searchedCity);
+  searchHistory.push(searchedCity);
+  localStorage.getItem("search", JSON.stringify(searchHistory));
+  renderSearchHistory();
+});
+
+// clear History
+clearHistoryBtn.on("click", function () {
+  searchHistory = [];
+  renderSearchHistory();
+});
+
+// convert Kelvin to Ferenheit
+function kelv2F(K) {
+  return Math.floor((K - 273.15) * 1.8 + 32);
+}
+
+// render search history
+function renderSearchHistory() {
+  previousCitiesUl.empty();
+  for (let i = 0; i < searchHistory.length; i++) {
+    var previousSearch = $("<input>", {
+      class: "form-control d-block bg-grey",
+    })
+      .attr("type", "text")
+      .attr("style", "margin-bottom: 10px;")
+      .attr("readonly", true)
+      .attr("value", searchHistory[i]);
+    previousSearch.on("click", function () {
+      getWeather(previousSearch.val());
+    });
+    previousCitiesUl.append(previousSearch);
+  }
+}
+renderSearchHistory();
+if (searchHistory.length > 0) {
+  getWeather(searchHistory[searchHistory.length - 1]);
+}
+
+// Weather !!
+function getWeather(cityName) {
   var apiKey = "d61e6c3f32b672ef640a1eeab500b0dc";
   var weatherApi =
-    `https://api.openweathermap.org/data/2.5/weather?q=denver&appid=` + apiKey;
+    `https://api.openweathermap.org/data/2.5/weather?q=` +
+    cityName +
+    `&appid=` +
+    apiKey;
   fetch(weatherApi)
     .then(function (response) {
       return response.json();
@@ -42,7 +87,8 @@ function getWeather() {
         "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
       );
       currentIcon.attr("alt", data.weather.description);
-      currentTemp.innerHTML = "Temperature: " + k2f(data.main.temp) + "&#176F";
+      currentTemp.innerHTML =
+        "Temperature: " + kelv2F(data.main.temp) + "&#176F";
       currentHum.innerHTML = "Humidity: " + data.main.humidity + "%";
       currentWind.innerHTML = "Wind Speed: " + data.main.speed + " MPH";
       var lat = response.data.coord.lat;
@@ -61,7 +107,55 @@ function getWeather() {
         }).text("UV Index: " + response.data[0].value);
         currentUV.append(uvIndex);
       });
+      // 5-day forcast
+      var cityId = response.daya.id;
+      var futureForcastURL =
+        "https://api.openweathermap.org/data/2.5/forecast?id=" +
+        cityId +
+        "&appid=" +
+        apiKey;
+
+      fetch(forcastURL).then(function (response) {
+        var futureForcasts = $(".forcast");
+        for (let i = 0; i < futureForcasts.length; i++) {
+          futureForcasts[i].empty();
+          var futureIndex = i * 8 + 4;
+          var futureDate = new Date(response.data.list[futureIndex].dt * 1000);
+          var futureDay = futureDate.getDate();
+          var futureMonth = futureDate.getMonth() + 1;
+          var futureYear = futureDate.getFullYear();
+          var futureDateEl = $("<p>", {
+            class: "mt-3 mb-0 forcast-date",
+          }).text(futureMonth + "/" + futureDay + "/" + futureYear);
+          futureForcasts.append(futureDateEl);
+          var futureWeatherEl = $("<img>", {
+            src:
+              `https://openweathermap.org/img/wn/` +
+              response.data.list[futureIndex].weather[0].icon +
+              `@2x.png`,
+          }).text(response.data.list[futureIndex].weather[0].description);
+          futureForcasts[i].append(futureWeatherEl);
+          //future Temp
+          var futureTempEl = $("<p>", {}).text(
+            "Temp: " +
+              kelv2F(response.data.list[futureIndex].main.temp) +
+              " &#176F"
+          );
+          futureForcasts[i].append(futureTempEl);
+
+          // future Wind
+          const futureWindEl = $("<p>", {}).text(
+            "Wind: " + response.data.list[futureIndex].wind.speed + " MPH"
+          );
+          futureForcasts[i].append(futureWindEl);
+
+          // future Humidity
+          const futureHumidityEl = $("<p>", {}).text(
+            "Humidity: " + response.data.list[futureIndex].main.humidity + "%"
+          );
+          futureForcasts[i].append(futureHumidityEl);
+        }
+      });
     });
 }
-
 getWeather();
