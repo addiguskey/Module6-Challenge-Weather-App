3dd6c73e884726fe913afb627a6d03b1ae5c411e
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-05-18 20:16:33 -0600

    first commit-nothing really working

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,65 @@
-var currentLoca = $("#current-location");
-var userCityInput = $("#user-city");
+var userCityInput = $("#user-city-input");
 // add eventlistner for usercitysubmit
-var userCitySubmit = $("user-city-submit");
-// use momentJS, display moment JS
+var userCitySearchBtn = $("user-city-submit");
+var clearHistoryBtn = $("clear-history");
 var previousCitiesUl = $("previous-cities");
-var futureForcast = $("future-forcast");
+var currentLoca = $("#current-city");
+var currentIcon = $("current-icon");
+var currentTemp = $("current-temp");
+var currentHum = $("current-humidity");
+var currentWind = $("current-wind");
+var currentUV = $("current-UV");
+// add eventlinster for submit btn
+//make an accnt for my key
+
+function getWeather() {
+  var apiKey = "d61e6c3f32b672ef640a1eeab500b0dc";
+  var weatherApi =
+    `https://api.openweathermap.org/data/2.5/weather?q=denver&appid=` + apiKey;
+  fetch(weatherApi)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var currentDate = new Date(data.dt * 1000);
+      var currentDay = currentDate.getDate();
+      var currentMonth = currentDate.getMonth() + 1;
+      var currentYear = currentDate.getFullYear();
+      currentLoca.innerHTML =
+        data.name +
+        "(" +
+        currentMonth +
+        "/" +
+        currentDay +
+        "/" +
+        currentYear +
+        ")";
+      let weatherIcon = data.weather.icon;
+      currentIcon.attr(
+        "src",
+        "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
+      );
+      currentIcon.attr("alt", data.weather.description);
+      currentTemp.innerHTML = "Temperature: " + k2f(data.main.temp) + "&#176F";
+      currentHum.innerHTML = "Humidity: " + data.main.humidity + "%";
+      currentWind.innerHTML = "Wind Speed: " + data.main.speed + " MPH";
+      var lat = response.data.coord.lat;
+      var lon = response.data.coord.lon;
+      var uvURL =
+        "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=" +
+        APIKey +
+        "&cnt=1";
+      fetch(uvURL).then(function (response) {
+        let uvIndex = $("<span>", {
+          class: "badge badge-danger",
+        }).text("UV Index: " + response.data[0].value);
+        currentUV.append(uvIndex);
+      });
+    });
+}
+
+getWeather();
