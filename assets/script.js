ceeec4b93e585ddbba33751a86cdb2ad12eae096
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-05-20 21:36:28 -0600

    holysh stuff is working

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,17 @@
 var today = moment();
 $("#todays-date").text(today.format("MM / D / YYYY"));
+
+var day1 = new moment().add(1, "day");
+$("#day1-d8").text(day1.format("MM/D/YYYY"));
+var day2 = new moment().add(2, "day");
+$("#day2-d8").text(day2.format("MM/D/YYYY"));
+var day3 = new moment().add(3, "day");
+$("#day3-d8").text(day3.format("MM/D/YYYY"));
+var day4 = new moment().add(4, "day");
+$("#day4-d8").text(day4.format("MM/D/YYYY"));
+var day5 = new moment().add(5, "day");
+$("#day5-d8").text(day5.format("MM/D/YYYY"));
+
 var userCityInput = $("#user-city-input");
 // add eventlistner for usercitysubmit
 // var searchBtn = $("#user-city-submit");
@@ -7,10 +19,12 @@ var clearHistoryBtn = $("#clear-history");
 var previousCitiesUl = $("#append-history");
 var appendCurrentLoca = $("#current-city");
 var appendCurrentIcon = $("#current-icon");
+var appendCurrentDescription = $("#current-description");
 var appendCurrentTemp = $("#current-temp");
 var appendCurrentHum = $("#current-humidity");
 var appendCurrentWind = $("#current-wind");
 var appendCurrentUV = $("#current-UV");
+
 var appendDay1 = $("#day1");
 var appendDay2 = $("#day2");
 var appendDay3 = $("#day3");
@@ -57,7 +71,7 @@ var apiKey = "d61e6c3f32b672ef640a1eeab500b0dc";
 // Weather !!
 function getWeather() {
   var searchedCity = userCityInput.val();
-  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${apiKey}`;
+  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${apiKey}&units=imperial`;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -72,71 +86,86 @@ function getWeather() {
         })
         .then(function (data) {
           console.log(data);
+          //
+          //   var requestIconURL = `http://openweathermap.org/img/wn/10d@2x.png`;
+          //   fetch(requestIconURL)
+          //     .then(function (response) {
+          //       return response.json();
+          //     })
+          //     .then(function (data) {
+          //       console.log(data);
+          //     });
           $("#current-city").append(searchedCity);
           var currentTemp = data.current.temp;
           var currentWind = data.current.wind_speed;
           var currentHum = data.current.humidity;
           var currentUV = data.current.uvi;
+          var currentDesciption = data.current.weather[0].description;
+          var currentIcon = data.current.weather[0].icon;
           appendCurrentTemp.text("Temperature: " + currentTemp);
           appendCurrentWind.text("Wind Speed: " + currentWind);
           appendCurrentHum.text("Humidity: " + currentHum);
           appendCurrentUV.text("UV Index: " + currentUV);
+          appendCurrentDescription.text(currentDesciption);
+          appendCurrentIcon.attr(currentIcon);
 
-          // DAY1
-          var day1Icon = data.daily[0].weather[0].id;
+          //   DAY1;
+          //   var day1Icon = data.daily[0].weather[0].icon;
           var day1Temp = data.daily[0].temp.day;
           var day1Wind = data.daily[0].wind_speed;
           var day1Hum = data.daily[0].humidity;
+          var day1Des = data.daily[0].weather[0].description;
 
-          $("#day1Icon").text(`${data.daily[0].weather[0].id}`);
-          $("#day1Temp").text(`${data.daily[0].temp.day}`);
-          $("#day1Wind").text(`${data.daily[0].wind_speed}`);
-          $("#day1UV").text(`${data.daily[0].humidity}`);
-          $("#day1Icon").append(day1Icon);
-          $("#day1Temp").append(day1Temp);
-          $("#day1Wind").append(day1Wind);
-          $("#day1Hum").append(day1Hum);
+          //   $("#day1Icon").attr("src", day1Icon);
+          $("#day1Des").text(day1Des);
+          $("#day1Temp").text("Temperature: " + day1Temp);
+          $("#day1Wind").text("Wind Speed: " + day1Wind);
+          $("#day1Hum").text("Humidity: " + day1Hum);
 
           // DAY2
-          var day2Icon = data.daily[1].weather[0].id;
+          var day2Icon = data.daily[1].weather[0].icon;
           var day2Temp = data.daily[1].temp.day;
           var day2Wind = data.daily[1].wind_speed;
           var day2Hum = data.daily[1].humidity;
+          var day2Des = data.daily[1].weather[0].description;
 
-          appendDay2.text(`${day2Icon}
-            Temp: ${day2Temp}
-            Wind: ${day2Wind}
-            Humidity:${day2Hum}`);
+          $("#day2Des").text(day2Des);
+          $("#day2Temp").text("Temperature: " + day2Temp);
+          $("#day2Wind").text("Wind Speed: " + day2Wind);
+          $("#day2Hum").text("Humidity: " + day2Hum);
           // DAY3
-          var day3Icon = data.daily[2].weather[0].id;
+          var day3Icon = data.daily[2].weather[0].icon;
           var day3Temp = data.daily[2].temp.day;
           var day3Wind = data.daily[2].wind_speed;
-          var day3Hum = data.daily[2].humidity;
+          var day3Hum = data.daily[2].humiconity;
+          var day3Des = data.daily[2].weather[0].description;
 
-          appendDay3.text(`${day3Icon}
-            Temp: ${day3Temp}
-            Wind: ${day3Wind}
-            Humidity:${day3Hum}`);
+          $("#day3Des").text(day3Des);
+          $("#day3Temp").text("Temperature: " + day3Temp);
+          $("#day3Wind").text("Wind Speed: " + day3Wind);
+          $("#day3Hum").text("Humidity: " + day3Hum);
           // DAY 4
-          var day4Icon = data.daily[3].weather[0].id;
+          var day4Icon = data.daily[3].weather[0].icon;
           var day4Temp = data.daily[3].temp.day;
           var day4Wind = data.daily[3].wind_speed;
           var day4Hum = data.daily[3].humidity;
+          var day4Des = data.daily[3].weather[0].description;
 
-          appendDay4.text(`${day4Icon}
-            Temp: ${day4Temp}
-            Wind: ${day4Wind}
-            Humidity:${day4Hum}`);
+          $("#day4Des").text(day4Des);
+          $("#day4Temp").text("Temperature: " + day4Temp);
+          $("#day4Wind").text("Wind Speed: " + day4Wind);
+          $("#day4Hum").text("Humidity: " + day4Hum);
           // DAY5
-          var day5Icon = data.daily[4].weather[0].id;
+          var day5Icon = data.daily[4].weather[0].icon;
           var day5Temp = data.daily[4].temp.day;
           var day5Wind = data.daily[4].wind_speed;
           var day5Hum = data.daily[4].humidity;
+          var day5Des = data.daily[4].weather[0].description;
 
-          appendDay5.text(`${day5Icon}
-            Temp: ${day5Temp}
-            Wind: ${day5Wind}
-            Humidity:${day5Hum}`);
+          $("#day5Des").text(day5Des);
+          $("#day5Temp").text("Temperature: " + day5Temp);
+          $("#day5Wind").text("Wind Speed: " + day5Wind);
+          $("#day5Hum").text("Humidity: " + day5Hum);
         });
     });
 }
